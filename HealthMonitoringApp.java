aa731da1c7b6685ebf32bffa5a830c0670ed7f02
Janeil Chantelle <132310787+janeilchantelle@users.noreply.github.com>
2024-04-23 09:37:08 -0230

    Updated comments.

--- HealthMonitoringApp.java
+++ HealthMonitoringApp.java
@@ -3,9 +3,7 @@ import java.util.List;
 public class HealthMonitoringApp {
     
     public static void main(String[] args) {
-        // Your main application logic here
         
-        // Example:
         UserDao userDao = new UserDao();
         
         User newUser = new User(1, "John", "Doe", "johndoe@example.com", "password123", false, false);
